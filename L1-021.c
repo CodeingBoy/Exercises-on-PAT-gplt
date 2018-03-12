fbe79d5bca0d0dfdc38d73ddd1019bbc49fa081c
CodeingBoy <CodeingBoy@gmail.com>
2018-03-12 15:34:42 +0800

    L1-021 重要的话说三遍 accepted

---
+++ L1-021.c
@@ -0,0 +1,12 @@
+// L1-021 重要的话说三遍
+// Language: C(C99)
+
+#include <stdio.h>
+
+int main(void){
+    printf("I'm gonna WIN!\n");
+    printf("I'm gonna WIN!\n");
+    printf("I'm gonna WIN!\n");
+    
+    return 0;
+}
